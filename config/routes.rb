fa12434646e1be4c9fc08d58f9aa5aa68c144cd0
Jose Salcido <eljose152@gmail.com>
2017-08-30 20:00:10 -0700

    errors

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   resources :mentors
   resources :students
 
+  get "/404" => "errors#not_found"
+  get "/500" => "errors#exception"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
